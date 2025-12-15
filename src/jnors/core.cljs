d9df5824329f3c632079fcee84a9deec260e0839
cl-journeyor <genericobject711@yahoo.com>
2025-12-15 11:03:05 -0800

    New fns

--- src/jnors/core.cljs
+++ src/jnors/core.cljs
@@ -1,6 +1,26 @@
 (ns jnors.core
   "Supplementary API.")
 
+(defn between?
+  "Checks whether `lower <= x <= upper` is true."
+  [x upper lower]
+  (and
+   (>= x lower)
+   (<= x upper)))
+
+(defn find-first
+  "Returns the first item from `coll` that matches the given `pred`."
+  [pred coll]
+  (->> coll
+       (filter pred)
+       first))
+
+(defn slice
+  "Retrieves a sequence from the item number `start` to number `endx`
+  exclusive, from `coll`."
+  [start endx coll]
+  (->> coll (drop start) (take (- endx start))))
+
 (defn throws?
   "Invokes the provided `f` to check whether it throws an exception."
   [f]
