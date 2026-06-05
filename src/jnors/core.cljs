005d573102ad896901dd752d7415d2af17ec1aec
cl-journeyor <genericobject711@yahoo.com>
2026-06-04 17:46:20 -0700

    core/effect-on-ineq

--- src/jnors/core.cljs
+++ src/jnors/core.cljs
@@ -8,6 +8,18 @@
    (>= x lower)
    (<= x upper)))
 
+(defn effect-on-ineq
+  "Takes a parameterless `proc`, presumably for side-effects, and a
+  parameterless `suppl`, and returns a new parameterless function that executes
+  `proc` the first time and when (suppl) not= the previously cached value."
+  [proc suppl]
+  (let [state (atom (js-obj))]
+    (fn []
+      (let [new-state (suppl)]
+        (when-not (= new-state @state)
+          (reset! state new-state)
+          (proc))))))
+
 (defn find-first
   "Returns the first item from `coll` that matches the given `pred`."
   [pred coll]
