5ad0fe9db16f33aede2d64768282674d459427e0
cl-journeyor <genericobject711@yahoo.com>
2026-06-08 15:07:10 -0700

    fetch abstraction with protocols and records

--- src/jnors/core.cljs
+++ src/jnors/core.cljs
@@ -1,6 +1,31 @@
 (ns jnors.core
   "Supplementary API.")
 
+;;;; Types.
+
+(defprotocol Else
+  "Specifies a type that can handle an alternative operation."
+  (else [this f] "Takes `f` as the alternative operation."))
+
+(defprotocol Then
+  "Specifies a type that can handle a primary operation."
+  (then [this f] "Takes `f` as the primary operation."))
+
+(defrecord FetchElse [url opts tfn]
+  Else (else [{:keys [url opts tfn]} f]
+         (-> (js/fetch url (clj->js opts))
+             (.then #(.json %))
+             (.then tfn)
+             (.catch f))
+         nil))
+
+(defrecord FetchThen [url opts]
+  Then (then [{:keys [url opts]} f]
+         (FetchElse. url opts f)))
+
+
+;;;; Functions.
+
 (defn between?
   "Checks whether `lower <= x <= upper` is true."
   [x lower upper]
@@ -19,6 +44,23 @@
         (when-not (= new-state @state)
           (reset! state new-state)
           (proc))))))
+
+(defn fetch
+  "Function pipeline to perform a JavaScript fetch to `url` with an `opts` map
+  to configure the request.
+   
+  The pipeline can be invoked like this:
+  ```
+  (-> (fetch \"https://site.io\" {:method \"GET\"})
+      (then #(println \"The jso:\" %))
+      (else #(println \"Network error:\" %)))
+  ```
+   
+  The else clause is mandatory as it evaluates the pipeline and returns nil."
+  ([url]
+   (fetch url nil))
+  ([url opts]
+   (FetchThen. url opts)))
 
 (defn find-first
   "Returns the first item from `coll` that matches the given `pred`."
